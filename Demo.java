af3834081ade79c52052942f2f9b548ec9627a02
kaustubhware <160700391+kaustubhware@users.noreply.github.com>
2024-02-23 09:48:48 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -4,8 +4,6 @@ public class Demo {
         int number1 = 5;
         int number2 = 6;
         int sum = number1 + number2;
-        
-       
         System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
     }
 }
