e02a83dc7abde8909e0718aa9a4abd1046a9573a
kaustubhware <160700391+kaustubhware@users.noreply.github.com>
2024-02-23 09:39:39 +0530

    Update and rename HelloWorld.java to Demo.java

---
+++ Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+    public static void main(String[] args) {
+      
+        int number1 = 5;
+        int number2 = 6;
+        
+        
+        int sum = number1 + number2;
+        
+       
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-class HelloWorld{
-  public static void main(String args[])
-  { 
-    System.out.println("Hello 1");
-    System.out.println("JAVA & JENKINS");
-  }
-}
