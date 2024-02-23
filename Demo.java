be786d7b916f8a59478629324362a943dc93e1e0
kaustubhware <160700391+kaustubhware@users.noreply.github.com>
2024-02-23 12:00:36 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
         int number1 = 5;
-        int number2 = 6;
+        int number2 = 6
         int sum = number1 + number2;
         System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
     }
